b92e9db35f687aa1b453d0a204828f9b6b53c1f0
amber-king <amberking@pursuit.org>
2023-05-03 12:21:21 -0400

    edits to editing review & other review add ons

--- front-end/src/Components/Review.js
+++ front-end/src/Components/Review.js
@@ -1,13 +1,28 @@
-function Review({ review }) {
-    return (
-      <div className="Review">
-        <h4>
-          {review.title} <span>{review.rating}</span>
-        </h4>
-        <h5>{review.reviewer}</h5>
-        <p>{review.content}</p>
-      </div>
-    );
-  }
-  
-  export default Review;
+import ReviewForm from "./ReviewForm";
+import { useState } from "react";
+function Review({ review, handleDelete, handleSubmit }) {
+  const [viewEditForm, toggleEditForm] = useState(false);
+
+  const toggleView = () => {
+    toggleEditForm(!viewEditForm);
+  };
+  return (
+    <div className="Review">
+      <button onClick={toggleView}>Edit this review</button>
+      {viewEditForm ? (
+        <ReviewForm reviewDetails={review} />
+      ) : (
+        <div>
+          <h4>
+            {review.title} <span>{review.rating}</span>
+          </h4>
+          <h5>{review.reviewer}</h5>
+          <p>{review.content}</p>
+        </div>
+      )}
+      <button onClick={() => handleDelete(review.id)}>delete</button>
+    </div>
+  );
+}
+
+export default Review;
